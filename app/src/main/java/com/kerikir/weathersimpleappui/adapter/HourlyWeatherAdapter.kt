f93362a43c1d0ba30941655c6f9f0394fe55a5a6
kerikir <vlasov-kerik@mail.ru>
2026-04-20 23:57:54 +0300

    added the adapter for Hourly Weather

---
+++ app/src/main/java/com/kerikir/weathersimpleappui/adapter/HourlyWeatherAdapter.kt
@@ -0,0 +1,32 @@
+package com.kerikir.weathersimpleappui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kerikir.weathersimpleappui.model.HourlyWeatherModel
+
+class HourlyWeatherAdapter(
+    private val items: ArrayList<HourlyWeatherModel>
+) : RecyclerView.Adapter<HourlyWeatherAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder {
+
+    }
+}
