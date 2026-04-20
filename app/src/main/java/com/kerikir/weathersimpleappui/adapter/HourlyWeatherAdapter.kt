e793ae70d3da9b460f223ab18e2538bea00224b0
kerikir <vlasov-kerik@mail.ru>
2026-04-21 00:20:00 +0300

    implemented adapter for recycler view for hourly weather

--- app/src/main/java/com/kerikir/weathersimpleappui/adapter/HourlyWeatherAdapter.kt
+++ app/src/main/java/com/kerikir/weathersimpleappui/adapter/HourlyWeatherAdapter.kt
@@ -1,32 +1,53 @@
 package com.kerikir.weathersimpleappui.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kerikir.weathersimpleappui.databinding.ViewholderHourlyWeatherBinding
 import com.kerikir.weathersimpleappui.model.HourlyWeatherModel
 
 class HourlyWeatherAdapter(
     private val items: ArrayList<HourlyWeatherModel>
 ) : RecyclerView.Adapter<HourlyWeatherAdapter.ViewHolder>() {
 
+    class ViewHolder(
+        val binding: ViewholderHourlyWeatherBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+        val binding = ViewholderHourlyWeatherBinding
+            .inflate(LayoutInflater.from(context), parent, false)
+        return HourlyWeatherAdapter.ViewHolder(binding)
     }
 
     override fun onBindViewHolder(
-        holder: ViewHolder,
+        holder: HourlyWeatherAdapter.ViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        holder.binding.apply {
+            hourHourly.text = item.hour
+            tempHourly.text = "${item.temperature}°"
+            val drawableResourceId = holder.itemView.resources.getIdentifier(
+                item.picPath,
+                "drawable",
+                context.packageName
+            )
+
+            Glide.with(context)
+                .load(drawableResourceId)
+                .into(picHourly)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    class ViewHolder {
-
-    }
+    override fun getItemCount(): Int = items.size
 }
