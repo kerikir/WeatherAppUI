e4bec9ee9f03a4077e47b7a9cbe92cbb0445dec3
kerikir <vlasov-kerik@mail.ru>
2026-04-21 21:55:22 +0300

    implemented the adapter for RecyclerView for cities weather

--- app/src/main/java/com/kerikir/weathersimpleappui/adapter/CityWeatherAdapter.kt
+++ app/src/main/java/com/kerikir/weathersimpleappui/adapter/CityWeatherAdapter.kt
@@ -1,30 +1,61 @@
 package com.kerikir.weathersimpleappui.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.kerikir.weathersimpleappui.databinding.ViewholderCityWeatherBinding
 import com.kerikir.weathersimpleappui.model.CityWeatherModel
 
 class CityWeatherAdapter(
     private val items: ArrayList<CityWeatherModel>
-) : RecyclerView.Adapter<CityWeatherAdapter.ViewHolder> {
+) : RecyclerView.Adapter<CityWeatherAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
 
     class ViewHolder(
         val binding: ViewholderCityWeatherBinding
     ) : RecyclerView.ViewHolder(binding.root)
 
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): CityWeatherAdapter.ViewHolder {
-        TODO("Not yet implemented")
+
+        context = parent.context
+        val binding = ViewholderCityWeatherBinding.inflate(
+            LayoutInflater.from(context), parent, false
+        )
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CityWeatherAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+    override fun onBindViewHolder(
+        holder: CityWeatherAdapter.ViewHolder,
+        position: Int
+    ) {
+
+        val item = items[position]
+        holder.binding.apply {
+            city.text = item.cityName
+            wind.text = "${item.wind} km/h"
+            humidity.text = "${item.humidity}%"
+            rain.text = "${item.rain}%"
+            temperatureCity.text = "${item.temperature}°"
+
+            val drawableResourceId = holder.itemView.resources.getIdentifier(
+                item.picPath,
+                "drawable",
+                context.packageName
+            )
+
+            Glide.with(context)
+                .load(drawableResourceId)
+                .into(picture)
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = items.size
 }
